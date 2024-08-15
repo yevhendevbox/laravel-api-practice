410bc0e8caa2bfa062c2a331823a0de2d4932b90
Yevhen Dovhan <y.d.devbox@gmail.com>
2024-08-15 15:49:48 +0300

    feat: added summary tasks initial endpoing

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,laravel-api.test',
+        'localhost,localhost:8000,localhost:5173,127.0.0.1,127.0.0.1:8000,127.0.0.1:5173,::1,laravel-api.test',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
--- routes/api/v2.php
+++ routes/api/v2.php
@@ -1,10 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V2\TaskController;
 use App\Http\Controllers\Api\V2\CompleteTaskController;
+use App\Http\Controllers\Api\V2\SummaryController;
+use App\Http\Controllers\Api\V2\TaskController;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->prefix('v2')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
+    Route::get('/summaries', SummaryController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
